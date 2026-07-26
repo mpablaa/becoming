cbcb34353035aa7f93dbcb1e597cced681022293
mpablaa <manjinder.pabla@hotmail.com>
2026-07-26 14:26:16 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "becoming-v2";
+const CACHE = "becoming-v3";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
